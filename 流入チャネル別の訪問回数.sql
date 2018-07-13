5b35435f2b399d0ab313b2a6b8ee941c95c5a264
DIAMOND DOGS <nakanishiyoshiaki0915@gmail.com>
2018-07-13 23:54:23 +0900

    Create 流入チャネル別の訪問回数.sql

---
+++ 流入チャネル別の訪問回数.sql
@@ -0,0 +1,11 @@
+
+UTMパラメータのutm_source、utm_mediumで判定
+　検索連動型広告
+　アフィリエイト
+　アドネットワーク
+
+ドメインで判定
+　検索エンジン（リファラ―ドメインが、search.yahoo.co.jp/www.google.co.jp）
+　ソーシャルメディア（リファラードメインが、twitter.com/www.facebook.com）
+　その他サイト（上記当てはまらないのはother）
+ 
