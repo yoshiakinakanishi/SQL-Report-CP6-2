541616b26fc09ad134c497f37aff1cdbb57ee9ab
DIAMOND DOGS <nakanishiyoshiaki0915@gmail.com>
2018-07-14 00:54:42 +0900

    Update 流入チャネル別の訪問回数.sql

--- 流入チャネル別の訪問回数.sql
+++ 流入チャネル別の訪問回数.sql
@@ -1,13 +1,13 @@
 
 UTMパラメータのutm_source、utm_mediumで判定
-　検索連動型広告
-　アフィリエイト
-　アドネットワーク
+ - 検索連動型広告
+ - アフィリエイト
+ - アドネットワーク
 
 ドメインで判定
-　検索エンジン（リファラ―ドメインが、search.yahoo.co.jp/www.google.co.jp）
-　ソーシャルメディア（リファラードメインが、twitter.com/www.facebook.com）
-　その他サイト（上記当てはまらないのはother）
+ - 検索エンジン（リファラ―ドメインが、search.yahoo.co.jp/www.google.co.jp）
+ - ソーシャルメディア（リファラードメインが、twitter.com/www.facebook.com）
+ - その他サイト（上記で当てはまらないのはother）
  
         stamp        | short_session | long_session |                                url                                 |             referrer
 ---------------------+---------------+--------------+--------------------------------------------------------------------+-----------------------------------
